8815ff33fda676f14111d78115030033e62e5c37
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-10-25 17:14:46 +0200

    Remove old implementation

--- src/Exception/Data/InvalidEnvironmentException.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Torr\PrismicApi\Exception\Data;
-
-use Torr\PrismicApi\Exception\PrismicApiException;
-
-final class InvalidEnvironmentException extends \InvalidArgumentException implements PrismicApiException
-{
-}
