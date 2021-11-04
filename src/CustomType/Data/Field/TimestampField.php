617a2c2e0e87ba5720ad7892ffdc938a2331b6ec
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-11-04 17:12:57 +0100

    Add Timestamp field

---
+++ src/CustomType/Data/Field/TimestampField.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Torr\PrismicApi\CustomType\Data\Field;
+
+/**
+ * @link https://prismic.io/docs/core-concepts/timestamp
+ */
+final class TimestampField extends InputField
+{
+	private const TYPE_KEY = "Timestamp";
+	public const DEFAULT_NOW = "now";
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct (
+		string $label,
+		?string $placeholder = null,
+		?string $default = null,
+	)
+	{
+		parent::__construct(self::TYPE_KEY, [
+			"label" => $label,
+			"placeholder" => $placeholder,
+			"default" => $default,
+		]);
+	}
+}
