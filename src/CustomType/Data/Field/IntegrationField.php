30da1a8d9f9c511c77d111b141e4f86f84ed435a
Jannik Zschiesche <j.zschiesche@21torr.com>
2021-11-04 17:52:31 +0100

    Add Integration field

---
+++ src/CustomType/Data/Field/IntegrationField.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Torr\PrismicApi\CustomType\Data\Field;
+
+/**
+ * @link https://prismic.io/docs/core-concepts/integration-fields
+ */
+final class IntegrationField extends InputField
+{
+	private const TYPE_KEY = "IntegrationFields";
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct (
+		string $label,
+		string $catalog,
+		?string $placeholder = null,
+	)
+	{
+		parent::__construct(self::TYPE_KEY, $this->filterOptionalFields([
+			"label" => $label,
+			"catalog" => $catalog,
+			"placeholder" => $placeholder,
+		]));
+	}
+}
